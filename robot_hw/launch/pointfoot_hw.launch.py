725fd45f276ba42cb588160637932f5915132389
jenkins_ci <jenkins_ci@limxdynamics.com>
2024-09-11 17:36:30 +0800

    1.1.5.20240911171022

--- robot_hw/launch/pointfoot_hw.launch.py
+++ robot_hw/launch/pointfoot_hw.launch.py
@@ -5,10 +5,18 @@ from launch_ros.actions import Node
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 import os
+import sys
 
 def generate_launch_description():
-    robot_controllers_model_file = PathJoinSubstitution([FindPackageShare("robot_controllers"), "config/pointfoot/policy/policy.onnx"])
-    robot_controllers_pointfoot_params_file = PathJoinSubstitution([FindPackageShare("robot_controllers"), "config/pointfoot/params.yaml"])
+    robot_type = os.getenv("ROBOT_TYPE")
+
+    # Check if the ROBOT_TYPE environment variable is set, otherwise exit with an error
+    if not robot_type:
+        print("Error: Please set the ROBOT_TYPE using 'export ROBOT_TYPE=<robot_type>'.")
+        sys.exit(1)
+
+    robot_controllers_model_file = PathJoinSubstitution([FindPackageShare("robot_controllers"), "config/pointfoot/"+ robot_type +"/policy/policy.onnx"])
+    robot_controllers_pointfoot_params_file = PathJoinSubstitution([FindPackageShare("robot_controllers"), "config/pointfoot/"+ robot_type +"/params.yaml"])
     robot_controllers_file = PathJoinSubstitution([FindPackageShare("robot_controllers"), "config/robot_controllers.yaml"])
     robot_hw_joystick_file = PathJoinSubstitution([FindPackageShare("robot_hw"), "config/joystick.yaml"])
     robot_hw_file = PathJoinSubstitution([FindPackageShare("robot_hw"), "config/robot_hw.yaml"])
